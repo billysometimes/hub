186eca89a6224960cb79ee5e2b9f35e08764b4c6
billysometimes <billywelch@gmail.com>
2014-03-10 12:06:49 -0700

    Delete example.dart
    adding examples

--- examples/example.dart
+++
@@ -1,9 +0,0 @@
-import '../bin/hub.dart' as Hub;
-
-main(){
-  String path = "home.html.hub";
-
-  var req = {"users":[{"name":"billy","age":30},{"name":"sally","age":38},{"name":"bob","age":22}],"thatNum":4,"treasure":{"map":true}};
-
-  Hub.renderFile(path,req,{"cache":false}).then((tmp)=>print(tmp));
-}
