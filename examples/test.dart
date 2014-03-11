83ebcfb6ddfa56d046ddda0b31685bafdaecb372
billysometimes <billywelch@gmail.com>
2014-03-10 19:43:25 -0700

    Delete test.dart
    deleted unnecessary files

--- examples/test.dart
+++
@@ -1,6 +0,0 @@
-main(){
-
-  print(escape("<&\""));
-}
-
-escape(html){html = html.replaceAll(r'&', '&amp;').replaceAll(r'<', '&lt;').replaceAll(r'>', '&gt;').replaceAll(new RegExp("'"), '&#39;').replaceAll(new RegExp('"'), "&quot;");return html;}
